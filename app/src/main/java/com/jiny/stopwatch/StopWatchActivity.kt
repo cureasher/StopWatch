0db95d4c9b950d0700754b4776e6a25ebc2ffbd9
cureasher <cureasher@gmail.com>
2022-10-24 17:38:10 +0900

    feat: initButton and subTime Function ADD StopWatchActivity.kt

--- app/src/main/java/com/jiny/stopwatch/StopWatchActivity.kt
+++ app/src/main/java/com/jiny/stopwatch/StopWatchActivity.kt
@@ -1,71 +1,111 @@
 package com.jiny.stopwatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.TextView
-import androidx.annotation.InspectableProperty.ValueType
+import androidx.appcompat.app.AppCompatActivity
 import com.jiny.stopwatch.databinding.ActivityStopwatchBinding
-import java.util.Timer
+import java.util.*
 
 class StopWatchActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityStopwatchBinding
+    private lateinit var binding: ActivityStopwatchBinding
     private var time = 0
+    private var subtime = 0
     private var isRunning = false
-    private var timerTask : Timer? = null
+    private var timerTask: Timer? = null
     private var lap = 1
+    private val recordList: ArrayList<String> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStopwatchBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-        with(binding){
+        with(binding) {
             startAndStopBT.setOnClickListener {
                 isRunning = !isRunning
-                if(isRunning) start() else pause()
+                if (isRunning) start() else pause()
             }
 
             recordBT.setOnClickListener {
-                if(time != 0) currentTapTime()
-            }
-        }
-    }
+                when (recordBT.text) {
+                    "구간기록" -> {
+                        if (time != 0) {
+                            currentTapTime()
+                            subtime = 0
+                        }
+                    }
+                    "초기화" -> {
+                        time = 0
+                        recordValueLayout.removeAllViews()
+                        lapLayout.removeAllViews()
+                        allTimeLayout.removeAllViews()
 
-    private fun start(){
-        binding.startAndStopBT.text = "중지"
-        binding.recordBT.text ="구간기록"
-        isRunning = true
-        timerTask = kotlin.concurrent.timer(period = 10){
-            time++
-            val hourTime = (time / 144000) % 24 // 1시간
-            val minuteTime = (time / 6000) % 60 // 1분
-            val secondTime = (time / 100) % 60 // 1초
-            val millisecondTime = time % 100 // 1밀리초
+                        allTimeHourTV.text = "00"
+                        allTimeMinuteTV.text = ":00"
+                        allTimeSecondTV.text = ":00"
+                        recordTimeHourTV.text = "00"
+                        recordTimeMinuteTV.text = ":00"
+                        recordTimeSecondTV.text = ":00"
+                    }
 
-            // 숫자 증가할때 00포맷으로 증가하는 코드
-            var hour = String.format("%02d", hourTime)
-            var minute = String.format("%02d", minuteTime)
-            var seconds = String.format("%02d", secondTime)
-            var milliseconds = String.format("%02d", millisecondTime)
-
-            runOnUiThread {
-                with(binding){
-                    allTimeHourTV.text = "$minute"
-                    allTimeMinuteTV.text = ":$seconds"
-                    allTimeSecondTV.text = ".$milliseconds"
                 }
             }
         }
     }
 
-    private fun pause(){
-        binding.startAndStopBT.text ="계속"
-        binding.recordBT.text ="초기화"
+    private fun start() {
+        binding.startAndStopBT.text = "중지"
+        binding.recordBT.text = "구간기록"
+        isRunning = true
+        timerTask = kotlin.concurrent.timer(period = 10) {
+            time++
+            subtime++
+            val hourTime = (time / 144000) % 24 // 1시간
+            val minuteTime = (time / 6000) % 60 // 1분
+            val secondTime = (time / 100) % 60 // 1초
+            val millisecondTime = time % 100 // 1밀리초
+
+            val subHourTime = (subtime / 144000) % 24 // 1시간
+            val subMinuteTime = (subtime / 6000) % 60 // 1분
+            val subSecondTime = (subtime / 100) % 60 // 1초
+
+            // 전체시간
+            var hour = String.format("%02d", hourTime)
+            var minute = String.format("%02d", minuteTime)
+            var seconds = String.format("%02d", secondTime)
+            var milliseconds = String.format("%02d", millisecondTime)
+
+            // 부분시간
+            var subhour = String.format("%02d", subHourTime)
+            var subminute = String.format("%02d", subMinuteTime)
+            var subseconds = String.format("%02d", subSecondTime)
+
+            recordList.add("$subhour:$subminute:$subseconds")
+
+
+            runOnUiThread {
+                with(binding) {
+                    allTimeHourTV.text = "$minute"
+                    allTimeMinuteTV.text = ":$seconds"
+                    allTimeSecondTV.text = ":$milliseconds"
+
+                    recordTimeHourTV.text = subhour
+                    recordTimeMinuteTV.text = ":$subminute"
+                    recordTimeSecondTV.text = ":$subseconds"
+                }
+            }
+//            recordList.add("$minute:$seconds:$milliseconds")
+        }
+    }
+
+    private fun pause() {
+        binding.startAndStopBT.text = "계속"
+        binding.recordBT.text = "초기화"
         timerTask?.cancel()
     }
 
-    private fun currentTapTime(){
+    private fun currentTapTime() {
         val lapTime = time
         val lapTimeIndex = TextView(this).apply {
             textSize = 20f
@@ -83,11 +123,23 @@ class StopWatchActivity : AppCompatActivity() {
         }
 
         lapTimeIndex.text = """${lap++}"""
-        recordTime.text = ""
-        allTime.text = """${String.format("%02d",(lapTime / 6000) % 60)}:${String.format("%02d",(lapTime / 100) % 60)}"""
+        recordTime.text = recordList.last()
+        allTime.text = """${String.format("%02d", (lapTime / 144000) % 24)}${
+            String.format(
+                "%02d",
+                (lapTime / 6000) % 60
+            )
+        }:${
+            String.format(
+                "%02d", (lapTime / 100) % 60
+            )
+        }"""
 
-        binding.lapLayout.addView(lapTimeIndex,0)
-        binding.recordValueLayout.addView(recordTime,0)
-        binding.allTimeLayout.addView(allTime,0)
+        // 초기화 버튼 : removeAllView
+
+
+        binding.lapLayout.addView(lapTimeIndex, 0)
+        binding.recordValueLayout.addView(recordTime, 0)
+        binding.allTimeLayout.addView(allTime, 0)
     }
 }
